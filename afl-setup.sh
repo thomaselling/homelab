a1c0f59fc52aee836c6dde7c853d83e58820376a
Thomas Elling <tee2103@columbia.edu>
2017-12-09 23:26:21 -0500

    add afl setup and rename scripts

---
+++ afl-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+# https://www.evilsocket.net/2015/04/30/fuzzing-with-afl-fuzz-a-practical-example-afl-vs-binutils/
+# afl setup and tutorial
+
+sudo apt-get install texinfo bison flex
+wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
+tar -xvzf afl-latest.tgz
+cd afl*
+make
+sudo make install
+
+cd
+git clone git://sourceware.org/git/binutils-gdb.git
+cd binutils*
+CC=afl-gcc ./configure
+make
+
+echo core > /proc/sys/kernel/core_pattern
+
+mkdir afl_in afl_out
+cp /bin/ps afl_in/
+#afl-fuzz -i afl_in -o afl_out ./binutils/readelf -a @@
